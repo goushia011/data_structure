c080cf89e2dcc123a50476c56768f192b8c64eb0
Goushia Parween <goushiaparween111@gmail.com>
2022-10-02 16:37:09 +0530

    implemented add node to position

--- linkedlist2.cpp
+++ linkedlist2.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node {
-    int data;
+struct Node
+{
+    int data, position;
     Node *next, *prev;
 };
 
@@ -11,82 +12,144 @@ struct List
     Node *head, *tail;
     int length;
 
-    List(){
-        head = tail=NULL;
-        length=0;
+    List()
+    {
+        head = tail = NULL;
+        length = 0;
     }
 
-    void display() {
-        Node *temp=head;
-        cout<<"List of "<<length<<" elements: ";
-        while(temp!=NULL){
-            cout<<temp->data<<" ";
-            temp=temp->next;
+    void display()
+    {
+        Node *temp = head;
+        cout << "List of " << length << " elements: ";
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
         }
-        cout<<endl;
+        cout << endl;
     }
-    Node* lastNode() {
-        Node *temp=head;
+    Node *lastNode()
+    {
+        Node *temp = head;
         while (temp->next != NULL)
         {
-            temp=temp->next;
+            temp = temp->next;
         }
         return temp;
-        
     }
 
-    void addNodeToEnd(int data) {
+    void addNodeToEnd(int data)
+    {
         Node *node = new Node;
         node->data = data;
-        node->next=NULL;
+        node->next = NULL;
         node->prev = tail;
 
-        if(head == NULL) {
-            head = tail= node;
-        } else {
+        if (length == 0)
+        {
+            head = tail = node;
+        }
+        else
+        {
             tail->next = node;
             tail = node;
         }
         length++;
     }
-    void addNodeToStart(int data) {
-        
-        Node *node=new Node;
-        node->data=data;
-        node->next=head;
-        node->prev=NULL;
-        head=node;
+    void addNodeToStart(int data)
+    {
+
+        Node *node = new Node;
+        node->data = data;
+        node->next = head;
+        node->prev = NULL;
+
+        if (length == 0)
+        {
+            head = tail = node;
+        }
+        else
+        {
+            head = node;
+        }
+
         length++;
     }
-    void addNodeToPositionn(int data, int position);
+    void addNodeToPosition(int data, int position)
+    {
+        if (position > length + 1) {
+            cout<<"Insertio failed! Position out of bound"<<endl;
+            return;
+        }
+        if(position == 1) {
+            addNodeToStart(data);
+        }
+        if(position == length + 1) {
+            addNodeToEnd(data);
+        }
+        Node *temp = head;
+        while (position-- > 1)
+        {
+            temp = temp->next;
+        }
+        Node *node = new Node;
+        node->data = data;
+        node->prev = temp->prev;
+        node->next = temp;
+        temp = temp->next;
+    }
 
-    void deleteNodeFromEnd(){
-        if(length <= 0){
-            cout<<"list is already empty";
+    void deleteNodeFromEnd()
+    {
+        if (length <= 0)
+        {
+            cout << "list is already empty";
             return;
         }
         Node *deleteThis = tail;
-        tail=tail->prev;
-        tail->next = NULL;
+        if (length == 1)
+        {
+            head = tail = NULL;
+        }
+        else
+        {
+            tail = tail->prev;
+            tail->next = NULL;
+        }
+
         delete deleteThis;
         length--;
     }
-    void deleteNodeFromStart() {
-        if(length <= 0){
-            cout<<"list is already empty";
+    void deleteNodeFromStart()
+    {
+        if (length <= 0)
+        {
+            cout << "list is already empty";
             return;
         }
-        Node *deleteThis=head;
-        head=head->next;
-        head->prev=NULL;
+        Node *deleteThis = head;
+
+        if (length == 1)
+        {
+            head = tail = NULL;
+        }
+        else
+        {
+            head = head->next;
+            head->prev = NULL;
+        }
+
         delete deleteThis;
         length--;
     }
     void deleteNodeFromPosition(int position);
 
-    void deallocate() {
+    void deallocate()
+    {
         Node *deleteThis, *temp = head;
-        while(temp != NULL) {
+        while (temp != NULL)
+        {
             deleteThis = temp;
             temp = temp->next;
             delete deleteThis;
@@ -94,7 +157,8 @@ struct List
     }
 };
 
-int main() {
+int main()
+{
     List list;
     list.addNodeToEnd(2);
     list.display();
@@ -109,6 +173,9 @@ int main() {
     list.display();
 
     list.deleteNodeFromEnd();
+    list.display();
+
+    list.addNodeToPosition(5, 2);
     list.display();
 
     list.deallocate();
