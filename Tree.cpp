0f0992ba219fdca806c113841fcacf455a4730c4
goushia011 <114407858+goushia011@users.noreply.github.com>
2022-10-09 17:03:40 +0530

    implemented tree (#1)
    implemented tree

---
+++ Tree.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+   
+void postorderTraversal(Node *node)
+{
+    if (node == NULL)
+        return;
+    else
+    {
+        postorderTraversal(node->left);
+        postorderTraversal(node->right);
+        cout << node->data << " ";
+    }
+}
+void inorderTraversal(Node *node)
+{
+    if (node == NULL)
+        return;
+    else
+    {
+        inorderTraversal(node->left);
+        cout << node->data <<" ";
+        inorderTraversal(node->right);
+    }
+}
+
+void preorderTraversal(Node *node)
+{
+    if (node == NULL)
+        return;
+    else
+    {
+        cout << node->data << " ";
+        inorderTraversal(node->left);
+        inorderTraversal(node->right);
+    }
+}
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(12);
+    root->right = new Node(9);
+    root->left->left = new Node(5);
+    root->left->right = new Node(6);
+    cout<<"hello"<<endl;
+
+    cout << "Inorder Traversal"
+         << " ";
+    inorderTraversal(root);
+    cout << endl;
+
+    cout << "postorder Traversal"
+         << " ";
+    postorderTraversal(root);
+    cout << endl;
+
+    cout << "preorder Traversal"
+         << " ";
+    preorderTraversal(root);
+    cout << endl;
+}
